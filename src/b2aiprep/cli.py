f01ba0c97079117da7d5a2898144fca719d5d676
Rahul Brito <rfbrito@g.harvard.edu>
2024-03-27 13:07:18 -0700

    fixed error in cli

--- src/b2aiprep/cli.py
+++ src/b2aiprep/cli.py
@@ -81,8 +81,8 @@ def batchconvert(csvfile, outdir, n_mels, n_coeff, compute_deltas, plugin, cache
     )
     
     with open(csvfile, "r") as f:
-    reader = csv.DictReader(f)
-    num_cols = len(reader.fieldnames)
+        reader = csv.DictReader(f)
+        num_cols = len(reader.fieldnames)
     lines = [line.strip() for line in f.readlines()]
 
     #parse csv file differently if it is one column 'filename'
