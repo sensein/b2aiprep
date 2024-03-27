566e10d07643b2d9b0d3232fa91dd87d11a49a4b
Satrajit Ghosh <satra@mit.edu>
2024-03-27 15:38:10 -0400

    fix: adjust tests with path determination

--- src/tests/test_speech_to_text.py
+++ src/tests/test_speech_to_text.py
@@ -1,34 +1,34 @@
-from b2aiprep.process import SpeechToText, Audio
+from pathlib import Path
+
 import pytest
 
-class TestSpeechToText:
+from b2aiprep.process import Audio, SpeechToText
+
+
+def test_transcribe():
     """
-    Tests for the SpeechToText class.
+    Validates SpeechToText's ability to convert audio to text accurately.
+    Checks if the transcription matches the expected output, considering known model discrepancies.
     """
-    
-    def test_transcribe(self):
-        """
-        Validates SpeechToText's ability to convert audio to text accurately.
-        Checks if the transcription matches the expected output, considering known model discrepancies.
-        """
-        audio_path = "../../data/vc_source.wav"
-        audio_content = "If it isn't, it isn't."
+    audio_path = str((Path(__file__).parent.parent.parent / "data/vc_source.wav").absolute())
+    audio_content = "If it isn't, it isn't."
 
-        # Note: Should be "If it didn't, it didn't.", but that's what the model understands
-        audio = Audio.from_file(audio_path)
+    # Note: Should be "If it didn't, it didn't.", but that's what the model understands
+    audio = Audio.from_file(audio_path)
 
-        speech_to_text = SpeechToText()
-        result = speech_to_text.transcribe(audio)
-        text = result["text"]
-        assert text.strip() == audio_content
+    speech_to_text = SpeechToText()
+    result = speech_to_text.transcribe(audio)
+    text = result["text"]
+    assert text.strip() == audio_content
 
-    def test_cuda_not_available(self):
-        """
-        Test behavior when CUDA is not available.
-        A ValueError should be raised.
 
-        To raise the exception, use "CUDA_VISIBLE_DEVICES="" pytest my_tests.py"
-        """
-        with pytest.raises(ValueError) as e:
-            SpeechToText(device="cuda")
-        assert str(e.value) == "CUDA is not available. Please use CPU."
+def test_cuda_not_available():
+    """
+    Test behavior when CUDA is not available.
+    A ValueError should be raised.
+
+    To raise the exception, use "CUDA_VISIBLE_DEVICES="" pytest my_tests.py"
+    """
+    with pytest.raises(ValueError) as e:
+        SpeechToText(device="cuda")
+    assert str(e.value) == "CUDA is not available. Please use CPU."
