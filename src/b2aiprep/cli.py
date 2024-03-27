5d7013b125f73184ea78235454c51b0f2c8b0417
Rahul Brito <rfbrito@g.harvard.edu>
2024-03-27 13:19:54 -0700

    debugging batch_csv, added the full code to the cli code insted of importing it as a library

--- src/b2aiprep/cli.py
+++ src/b2aiprep/cli.py
@@ -1,6 +1,8 @@
 import os
 import typing as ty
 from pathlib import Path
+import csv
+from glob import glob
 
 import click
 import pydra
@@ -8,7 +10,6 @@ from pydra.mark import annotate
 from pydra.mark import task as pydratask
 
 from .process import to_features, verify_speaker_from_files
-from .create_batch_csv import create_batch_csv
 
 
 @click.group()
@@ -133,5 +134,20 @@ def verify(file1, file2, model, device=None):
 @click.argument("input_dir", type=str)
 @click.argument("out_file", type=str)
 def create_batch_csv(input_dir, out_file):
-    create_batch_csv(input_dir, out_file)
+    
+    #input_dir is the top level directory of the b2ai Production directory from Wasabi
+    #it is expected to contain subfolders with each institution e.g. MIT, UCF, etc.
+    
+    #out_file is where a csv file will be saved and should be in the format 'path/name/csv'
+    
+    audiofiles = glob(f'{input_dir}/*/*.wav')
+
+    with open(out_file, 'w') as f:
+
+        # using csv.writer method from CSV package
+        write = csv.writer(f)
+
+        for item in audiofiles:
+            write.writerow([item])
+            
     print(f"csv of audiofiles generated at: {out_file}")
