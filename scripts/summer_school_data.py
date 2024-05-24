fc81cbf818a742a89e7b818634931d60aa532c2c
ibevers <isaacbevers@gmail.com>
2024-05-24 11:23:06 -0400

    Initiate script for Summer School data organization

---
+++ scripts/summer_school_data.py
@@ -0,0 +1,35 @@
+"""
+Organizes data, extracts features, and bundles everything together in an easily distributable
+format for the Bridge2AI Summer School.
+"""
+
+
+def retrieve_data():
+    # TODO implement
+    return
+
+
+def organize_data():
+    # TODO implement
+    return
+
+
+def extract_features():
+    # TODO implement
+    return
+
+
+def bundle_data(data, save_path):
+    # TODO implement
+    return
+
+
+def main():
+    retrieve_data()
+    organize_data()
+    extract_features()
+    bundle_data()
+
+
+if __name__ == "__main__":
+    main()
