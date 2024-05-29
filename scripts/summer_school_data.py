f6e2c13662220b7ca87caa7e5fc01ffdb4d31c81
ibevers <isaacbevers@gmail.com>
2024-05-29 14:11:29 -0400

    Add to-dos and import b2aiprep feature extraction functions

--- scripts/summer_school_data.py
+++ scripts/summer_school_data.py
@@ -3,6 +3,18 @@ Organizes data, extracts features, and bundles everything together in an easily 
 format for the Bridge2AI Summer School.
 """
 
+from b2aiprep.process import (
+    embed_speaker,
+    verify_speaker, #maybe exclude?
+    verify_speaker_from_files, #maybe exclude?
+    specgram,
+    melfilterbank,
+    MFCC,
+    resample_iir,
+    extract_opensmile,
+    VoiceConversion,
+    SpeechToText
+)
 
 def retrieve_data():
     # TODO implement
@@ -16,6 +28,18 @@ def organize_data():
 
 def extract_features():
     # TODO implement
+    """
+    - Current: 
+         - opensmile (one vector per audio), 
+         - spectrograms, 
+         - mel filerbanks, 
+         - mfccs, 
+         - text transcripts
+    - Can we detect if certain segments of voice contain identifiable information: https://github.com/sensein/b2aiprep/issues/51
+    - Quality metrics (SNR, intelligibility)
+    - Praat extraction
+    - Some items from Nick feature table
+    """
     return
 
 
