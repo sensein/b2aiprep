f720bea326ceffb9e7fad84ecdb83c73a7dfa227
ibevers <isaacbevers@gmail.com>
2024-05-30 17:45:52 -0400

    Add extract features function

---
+++ scripts/run_summer_school_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /Users/isaacbevers/sensein/b2ai-wrapper/b2aiprep/scripts/summer_school_data.py --data_path /Users/isaacbevers/sensein/b2ai-wrapper/output --tar_save_path ./summer_school_data_test_bundle.tar
--- scripts/summer_school_data.py
+++ scripts/summer_school_data.py
@@ -3,12 +3,18 @@ Organizes data, extracts features, and bundles everything together in an easily 
 format for the Bridge2AI Summer School.
 """
 
-import os
 import argparse
+import os
+import shutil
 import tarfile
 
+import torch
+
+
+PARTICIPANT_DIGITS = 5  # 30,000 target participants
+
 from b2aiprep.process import (
-    #embed_speaker,
+    embed_speaker,
     specgram,
     melfilterbank,
     MFCC,
@@ -17,14 +23,14 @@ from b2aiprep.process import (
 )
 
 
-def retrieve_data():
+def retrieve_data(data_path):
     # TODO implement
     return
 
 
-def organize_data():
+def organize_data(data_path):
     # TODO implement
-    return
+    return data_path
 
 
 def wav_to_features(wav_path):
@@ -43,32 +49,40 @@ def wav_to_features(wav_path):
     return features
 
 
-def extract_subject_features(subject_path):
+def extract_features(processed_files_path):
     """
     Extracts features for every activity audio file.
+
     Args:
+        processed_files_path (str): The path to the directory saved as a .tar.
+
     Returns:
-        features_pt: extracted features
+        None
     """
-
-    # for wav file in subject dir
-        # extract audio features
-        # determine save path
-        # save features
-
-    #data structure
-    # if subject is not None:
-    #     if task is not None:
-    #         prefix = f"sub-{subject}_task-{task}_md5-{md5sum}"
-    #     else:
-    #         prefix = f"sub-{subject}_md5-{md5sum}"
-    # else:
-    #     prefix = Path(filename).stem
-    # if outdir is None:
-    #     outdir = Path(os.getcwd())
-    # outfile = outdir / f"{prefix}_features.pt"
-    # torch.save(features, outfile)
-    pass
+    # Iterate over each subject directory.
+    for sub_file in os.listdir(processed_files_path):
+        sub_file_path = os.path.join(processed_files_path, sub_file)
+        if sub_file.startswith("sub") and os.path.isdir(sub_file_path):
+            
+            # Iterate over each session directory within a subject.
+            for ses_file in os.listdir(sub_file_path):
+                voice_path = os.path.join(sub_file_path, ses_file, "voice")
+                if ses_file.startswith("ses") and os.path.isdir(voice_path):
+                    
+                    # Iterate over each audio file in the voice directory.
+                    for audio_file in os.listdir(voice_path):
+                        if audio_file.endswith('.wav'):
+                            audio_path = os.path.join(voice_path, audio_file)
+                            
+                            # Extract features from the audio file.
+                            features = wav_to_features(audio_path)
+                            
+                            # Remove the original audio file.
+                            os.remove(audio_path)
+                            
+                            # Save the extracted voice features.
+                            save_path = audio_path[:-len(".wav")] + ".pt"
+                            torch.save(features, save_path)
 
 
 def bundle_data(source_directory, save_path):
@@ -83,17 +97,47 @@ def parse_arguments():
                         type=str,
                         required=True,
                         help='Path to data in BIDS-like format')
+    parser.add_argument('--tar_save_path',
+                        type=str,
+                        required=True,
+                        help='Where to save the data.')
+    parser.add_argument('--processed_files_path',
+                        type=str,
+                        required=False,
+                        default="summer-school-processed-data",
+                        help='Where to save the data.')
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    for dir in os.listdir(args.data_path):
-        # check if dir is a subject
-        retrieve_data(args.data_path)
-        organize_data()
-        extract_subject_features()
-        bundle_data()
+
+    # copy data to directory for tar file
+    if os.path.isdir(args.processed_files_path):
+        shutil.rmtree(args.processed_files_path)
+    shutil.copytree(args.data_path, args.processed_files_path)
+
+    retrieve_data(args.data_path)
+    organize_data(args.data_path)
+    extract_features(args.processed_files_path)
+    # for sub_file in os.listdir(args.processed_files_path):  # iterate over subjects
+    #     if sub_file.startswith("sub"):
+    #         for ses_file in os.listdir(sub_file):  # iterate over sessions
+    #             if ses_file.startswith("ses") and os.path.isdir(f"{ses_file}/voice"): 
+    #                 for audio_file in os.listdir(f"{ses_file}/voice"):
+    #                    if audio_file.endswith('.wav'):
+    #                        print(audio_file)
+                           #do something
+                    
+            # subject_num_padded = str(subject_num).zfill(PARTICIPANT_DIGITS)
+            # os.mkdir(f"{args.processed_files_path}/sub-{subject_num_padded}")
+            # subject_num += 1
+            #create 
+            #iterate over .wav files
+            #
+            # print(file)
+            # extract_subject_features()
+            # bundle_data()
 
 
 if __name__ == "__main__":
