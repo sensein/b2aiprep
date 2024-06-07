e31b06849dfd7b0b8aafa8e6d233e71d38ca048c
ibevers <isaacbevers@gmail.com>
2024-06-07 15:16:21 -0400

    Remove separate optimized and non-optimized files and clean up

--- src/b2aiprep/summer_school_data.py
+++ src/b2aiprep/summer_school_data.py
@@ -2,6 +2,8 @@
 Organizes data, extracts features, and bundles everything together in an
 easily distributable format for the Bridge2AI Summer School.
 
+Feature extraction is parallelized using Pydra.
+
 Example usage:
 python3 b2aiprep/scripts/summer_school_data.py \
     --ftf_redcap_file_path bridge2ai-voice-corpus-1-ftf/bridge2ai_voice_data.csv \
@@ -17,7 +19,6 @@ import tarfile
 
 import torch
 import pydra
-# from senselab.audio.tasks.speech_to_text import transcribe_dataset
 from pathlib import Path
 
 from b2aiprep.prepare import redcap_to_bids
@@ -30,19 +31,26 @@ from b2aiprep.process import (
     Audio,
 )
 
-logging.basicConfig(level=logging.INFO)
+SUBJECT_ID = "sub"
+SESSION_ID = "ses"
+AUDIO_ID = "audio"
+AUDIO_FILE_EXTENSION = ".wav"
+
 _logger = logging.getLogger(__name__)
 
+
+@pydra.mark.task
 def wav_to_features(wav_path):
     """
     Extracts features from a single audio file at specified path.
     """
-    logging.disable(logging.WARNING)
-    audio = Audio.from_file(str(wav_path))
+    _logger.info(wav_path)
+    logging.disable(logging.ERROR)
+    print(len(wav_path), wav_path[-50:])
+    audio = Audio.from_file(wav_path)
     audio = audio.to_16khz()
+
     features = {}
-    # features["transcription"] = transcribe_dataset({"audio": audio}, model_id="openai/whisper-tiny")
-    # _logger.info(features["transcription"])
     features["speaker_embedding"] = embed_speaker(
         audio,
         model="speechbrain/spkrec-ecapa-voxceleb"
@@ -54,47 +62,65 @@ def wav_to_features(wav_path):
     features["opensmile"] = extract_opensmile(audio)
     logging.disable(logging.NOTSET)
     _logger.setLevel(logging.INFO)
+
+    save_path = wav_path[:-len(".wav")] + ".pt"
+    torch.save(features, save_path)
     return features
 
 
-def extract_features(processed_files_path, remove=False):
-    """
-    Extracts features for every activity audio file.
+@pydra.mark.task
+def get_audio_paths(bids_dir_path):
+    audio_paths = []
 
-    Args:
-        processed_files_path (str): The path to the directory saved as a .tar.
-
-    Returns:
-        None
-    """
     # Iterate over each subject directory.
-    for sub_file in os.listdir(processed_files_path):
-        sub_file_path = os.path.join(processed_files_path, sub_file)
-        if sub_file.startswith("sub") and os.path.isdir(sub_file_path):
+    for sub_file in os.listdir(bids_dir_path):
+        subject_dir_path = os.path.join(bids_dir_path, sub_file)
+        if sub_file.startswith(SUBJECT_ID) and os.path.isdir(subject_dir_path):
        
             # Iterate over each session directory within a subject.
-            for ses_file in os.listdir(sub_file_path):
-                voice_path = os.path.join(sub_file_path, ses_file, "audio")
-                _logger.info(voice_path)
-                if ses_file.startswith("ses") and os.path.isdir(voice_path):
+            for session_dir_path in os.listdir(subject_dir_path):
+                audio_path = os.path.join(subject_dir_path, session_dir_path, AUDIO_ID)
+                _logger.info(audio_path)
+                if session_dir_path.startswith(SESSION_ID) and os.path.isdir(audio_path):
                     
                     # Iterate over each audio file in the voice directory.
-                    for audio_file in os.listdir(voice_path):
-                        if audio_file.endswith('.wav'):
-                            audio_path = os.path.join(voice_path, audio_file)
-                            
-                            # Extract features from the audio file.
-                            _logger.info(f"Extracting features: {audio_file}")
-                            features = wav_to_features(audio_path)
-                            
-                            # Remove the original audio file.
-                            if remove:
-                                os.remove(audio_path)
-                            
-                            # Save the extracted voice features.
-                            save_path = audio_path[:-len(".wav")] + ".pt"
-                            torch.save(features, save_path)
+                    for audio_file in os.listdir(audio_path):
+                        if audio_file.endswith(AUDIO_FILE_EXTENSION):
+                            audio_file_path = os.path.join(audio_path, audio_file)
+                            audio_paths.append(audio_file_path)
+    return audio_paths
 
+def extract_features_workflow(bids_dir_path, remove=True):
+
+    # Initialize the Pydra workflow.
+    ef_wf = pydra.Workflow(
+        name="ef_wf",
+        input_spec=["bids_dir_path"],
+        bids_dir_path=bids_dir_path
+    )
+
+    # Get paths to every audio file.
+    ef_wf.add(get_audio_paths(
+            name="audio_paths",
+            bids_dir_path=bids_dir_path
+        )
+    )
+
+    # Run wav_to_features for each audio file in parallel.
+    ef_wf.add(wav_to_features(
+            name="features",
+            wav_path=ef_wf.audio_paths.lzout.out
+        ).split(
+            "wav_path",
+            wav_path=ef_wf.audio_paths.lzout.out
+        )
+    )
+
+    ef_wf.set_output({"audio_paths": ef_wf.audio_paths.lzout.out,
+                      "features": ef_wf.features.lzout.out})
+    with pydra.Submitter(plugin='cf') as run:
+        run(ef_wf)
+    return ef_wf
 
 def bundle_data(source_directory, save_path):
     """Saves data bundle as a tar file with gzip compression."""
@@ -102,52 +128,43 @@ def bundle_data(source_directory, save_path):
         tar.add(source_directory, arcname=os.path.basename(source_directory))
 
 
-# def parse_arguments():
-#     parser = argparse.ArgumentParser(
-#         description="Process audio data for multiple subjects."
-#     )
-#     parser.add_argument('--ftf_redcap_file_path',
-#                         type=Path,
-#                         required=True,
-#                         help='Path to the RedCap data file.')
-#     parser.add_argument('--audio_dir_path',
-#                         type=Path,
-#                         required=True,
-#                         help='Path to the audio files directory.')
-#     parser.add_argument('--tar_file_path',
-#                         type=Path,
-#                         required=True,
-#                         help='Where to save the data bundle .tar file.')
-#     parser.add_argument('--bids_files_path',
-#                         type=Path,
-#                         required=False,
-#                         default="summer-school-processed-data",
-#                         help='Where to save the BIDS-like data.')
-#     return parser.parse_args()
-
-class parse_arguments():
-    def __init__(self) -> None:
-        self.updated_redcap_file_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data/Bridge2AIPRODUCTION-DataDissemination_DATA_LABELS_2024-06-04_0959.csv")
-        self.audio_dir_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data/audio-data")
-        self.tar_file_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data/summer_school_data_test_bundle.tar")
-        self.bids_files_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data-bids-like")
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Process audio data for multiple subjects."
+    )
+    parser.add_argument('--audio_dir_path',
+                        type=Path,
+                        required=True,
+                        help='Path to the audio files directory.')
+    parser.add_argument('--tar_file_path',
+                        type=Path,
+                        required=True,
+                        help='Where to save the data bundle .tar file.')
+    parser.add_argument('--bids_files_path',
+                        type=Path,
+                        required=False,
+                        default="summer-school-processed-data",
+                        help='Where to save the BIDS-like data.')
+    return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-
+    logging.basicConfig(level=logging.INFO)
 
     _logger.info("Organizing data into BIDS-like directory structure...")
     redcap_to_bids(args.updated_redcap_file_path,
                    args.bids_files_path,
                    args.audio_dir_path)
-    # _logger.info("Extracting acoustic features from audio...")
-    # extract_features(args.bids_files_path, remove=False)
-    # _logger.info("Extracting features in ")
+    _logger.info("Data organization complete.")
 
-    # _logger.info("Saving .tar file with processed data...")
-    # bundle_data(args.bids_files_path, args.tar_file_path)
-    # _logger.info(f"Saved .tar file with processed data at {args.tar_file_path}")
+    _logger.info("Beginning audio feature extraction...")
+    extract_features_workflow(args.bids_files_path, remove=False)
+    _logger.info("Audio feature extraction complete.")
+
+    _logger.info("Saving .tar file with processed data...")
+    bundle_data(args.bids_files_path, args.tar_file_path)
+    _logger.info(f"Saved .tar file with processed data at: {args.tar_file_path}")
 
     _logger.info("Process completed.")
 
--- tests/test_summer_school_data.py
+++ tests/test_summer_school_data.py
@@ -1,40 +1,41 @@
-import time
-import os
-from pathlib import Path
+import pytest
 import logging
-from b2aiprep.summer_school_data import extract_features
-
+from b2aiprep.summer_school_data import (
+    extract_features_workflow
+)
 
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger(__name__)
 
 
-class parse_arguments():
-    def __init__(self) -> None:
-        self.ftf_redcap_file_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data/bridge2ai-voice-corpus-1/bridge2ai_voice_data.csv")
-        self.updated_redcap_file_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data/Bridge2AIDEVELOPMENT_DATA_2024-06-03_1705.csv")
-        self.audio_dir_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data/audio-data")
-        self.tar_file_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data/summer_school_data_test_bundle.tar")
-        self.bids_files_path=Path("/Users/isaacbevers/sensein/b2ai-wrapper/b2ai-data-bids-like")
+def pytest_addoption(parser):
+    parser.addoption(
+        "--bids-files-path", 
+        action="store", 
+        default=None, 
+        help="Path to BIDS files"
+    )
 
-args = parse_arguments()
+@pytest.fixture
+def bids_files_path(request):
+    return request.config.getoption("--bids-files-path")
 
 
 def test_wav_to_features_output():
     """Tests that the output is formatted correctly."""
     #TODO: implement
-    return
+    assert False, "Not implemented."
 
 
 def test_extract_features_coverage():
     """Tests that extract features extracted features for every audio file."""
     #TODO: implement
-    return
+    assert False, "Not implemented."
 
 
-def test_extract_features_timing(benchmark, caplog, ):
+def test_extract_features_timing(benchmark, caplog, bids_files_path):
     """Times the non-optimized iterative extract_features function."""
-    result = benchmark(extract_features, args.bids_files_path)
+    result = benchmark(extract_features_workflow, bids_files_path)
     caplog.set_level(logging.INFO)
     _logger.info(str(result))
     return
