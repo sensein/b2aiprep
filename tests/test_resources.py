aafa96ab23ab4649ed08c0317e20cf6b538ffca7
ibevers <isaacbevers@gmail.com>
2024-08-29 12:49:17 -0400

    Fix out of date path in test

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -8,6 +8,8 @@ from b2aiprep.prepare.constants import GENERAL_QUESTIONNAIRES
 def test_verify_all_resources_are_present():
     """Verify that all resource JSON files are present in the instrument_columns folder."""
 
-    b2ai_resources = files("b2aiprep").joinpath("resources").joinpath("instrument_columns")
+    b2ai_resources = (
+        files("b2aiprep").joinpath("prepare").joinpath("resources").joinpath("instrument_columns")
+    )
     for questionnaire_name in GENERAL_QUESTIONNAIRES:
         assert b2ai_resources.joinpath(f"{questionnaire_name}.json").exists()
