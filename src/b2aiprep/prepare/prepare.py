15781ddd39d9cfef439d32dd22ed3fb8ba3869e5
Satrajit Ghosh <satra@mit.edu>
2024-11-13 11:37:32 -0500

    fix: force multiprocessing spawn

--- src/b2aiprep/prepare/prepare.py
+++ src/b2aiprep/prepare/prepare.py
@@ -247,6 +247,9 @@ def extract_features_workflow(
       pydra.Task:
         The Pydra Task object with the extracted feature paths.
     """
+    import multiprocessing as mp
+
+    mp.set_start_method("spawn")
     if n_cores > 1:
         plugin_args: dict = {"n_procs": n_cores} if plugin == "cf" else {}
     else:
