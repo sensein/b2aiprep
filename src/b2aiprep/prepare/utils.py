f6eda1ad18448e75420ed2ebaba0dff7aaf9d6a8
ibevers <isaacbevers@gmail.com>
2025-03-18 17:13:17 -0400

    Fix import problem

--- src/b2aiprep/prepare/utils.py
+++ src/b2aiprep/prepare/utils.py
@@ -15,7 +15,7 @@ import pandas as pd
 _LOGGER = logging.getLogger(__name__)
 
 
-def transform_str_for_bids_filename(filename: str):
+def _transform_str_for_bids_filename(filename: str):
     """Replace spaces in a string with hyphens to match BIDS string format rules.."""
     return filename.replace(" ", "-")
 
