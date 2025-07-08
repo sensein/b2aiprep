ab2d694ce2884bb353a20cf01713bbfca03418af
Evan8456 <evanng8456@gmail.com>
2025-07-08 15:31:44 -0400

    added test cases for each function

--- tests/test_validate_data.py
+++ tests/test_validate_data.py
@@ -6,6 +6,13 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
+from b2aiprep.prepare.data_validation import (
+    validate_derivatives,
+    validate_participant_data,
+    clean_number,
+    get_choices
+)
+
 
 @pytest.fixture
 def setup_temp_files():
@@ -15,27 +22,27 @@ def setup_temp_files():
 
     # Synthetic record
     synthetic_record = {"record_id": "test_0001",
-                         "height": 170,
-                         "weight": 80,
-                         "unit": "Metric",
-                         "nervous_anxious": "Not at all",
-                         "cant_control_worry": "Not at all",
-                         "worry_too_much": "Several days",
-                         "trouble_relaxing": "Several days",
-                         "hard_to_sit_still": "Several days",
-                         "easily_agitated": "Several days",
-                         "afraid_of_things": "Several days",
-                         "tough_to_work": "Not difficult at all"}
+                        "height": 170,
+                        "weight": 80,
+                        "unit": "Metric",
+                        "nervous_anxious": "Not at all",
+                        "cant_control_worry": "Not at all",
+                        "worry_too_much": "Several days",
+                        "trouble_relaxing": "Several days",
+                        "hard_to_sit_still": "Several days",
+                        "easily_agitated": "Several days",
+                        "afraid_of_things": "Several days",
+                        "tough_to_work": "Not difficult at all"}
     df = pd.DataFrame([synthetic_record])
 
     # Save sythetic df into temp directory
     redcap_csv_path = os.path.join(tempfile.gettempdir(), "sdv_redcap_synthetic_data.csv")
     df.to_csv(redcap_csv_path, index=False)
-   
+
     yield redcap_csv_path
 
 
-def test_prepare_bids_cli(setup_temp_files):
+def test_validate_data_cli(setup_temp_files):
     """Test the 'b2aiprep-cli prepbidslikedata' command using subprocess."""
     redcap_csv_path = setup_temp_files
 
@@ -51,3 +58,65 @@ def test_prepare_bids_cli(setup_temp_files):
 
     # Check if the command was successful
     assert result.returncode == 0, f"CLI command failed: {result.stderr}"
+
+
+def test_validate_derivatives(setup_temp_files):
+    redcap_csv_path = setup_temp_files
+    assert validate_derivatives(redcap_csv_path) is None
+
+
+def test_validate_participant_data():
+    synthetic_record = {"record_id": "test_0001",
+                        "height": 170,
+                        "weight": 80,
+                        "unit": "Metric",
+                        "nervous_anxious": "Not at all",
+                        "cant_control_worry": "Not at all",
+                        "worry_too_much": "Several days",
+                        "trouble_relaxing": "Several days",
+                        "hard_to_sit_still": "Several days",
+                        "easily_agitated": "Several days",
+                        "afraid_of_things": "Several days",
+                        "tough_to_work": "Not difficult at all"}
+    assert validate_participant_data(synthetic_record) is None
+
+
+def test_clean_number():
+    assert clean_number("5") == 5
+    assert clean_number("test") == "test"
+    assert clean_number(5.0) == 5
+    assert clean_number(3.14) == 3.14
+
+
+def test_get_choices():
+    choices = [
+        {
+            "name": {
+                "en": "Not at all"
+            },
+            "value": 0
+        },
+        {
+            "name": {
+                "en": "Several days"
+            },
+            "value": 1
+        },
+        {
+            "name": {
+                "en": "More than half the days"
+            },
+            "value": 2
+        },
+        {
+            "name": {
+                "en": "Nearly every day"
+            },
+            "value": 3
+        }
+    ]
+
+    actual = get_choices(choices=choices)
+    expected = {"Not at all","Several days", "More than half the days", "Nearly every day"}
+
+    assert actual == expected
