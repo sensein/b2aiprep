00c89106474fe116c9d6dcf7cd35aff131eafc25
Evan8456 <evanng8456@gmail.com>
2025-07-08 13:20:53 -0400

    Fixed test case

--- tests/test_prepare.py
+++ tests/test_prepare.py
@@ -269,15 +269,15 @@ def test_update_metadata_record_and_session_id():
     assert metadata == expected_metadata
 
 
-# @pytest.mark.skipif(
-#     os.getenv("CI") == "true", reason="Skipping benchmarking test in CI environment"
-# )
-# def test_extract_features_timing(benchmark, caplog, bids_files_path):
-#     caplog.set_level(logging.INFO)
-#     if bids_files_path is None:
-#         _logger.error("Please provide the path to BIDS files using --bids-files-path")
-#         return
-#     result = benchmark(extract_features_workflow, bids_files_path)
-#     _logger.info(str(result))
-#     assert result is not None, "Benchmark failed"
-#     assert len(result["features"]) > 0, "No features extracted"
+@pytest.mark.skipif(
+    os.getenv("CI") == "true", reason="Skipping benchmarking test in CI environment"
+)
+def test_extract_features_timing(benchmark, caplog, bids_files_path):
+    caplog.set_level(logging.INFO)
+    if bids_files_path is None:
+        _logger.error("Please provide the path to BIDS files using --bids-files-path")
+        return
+    result = benchmark(extract_features_workflow, bids_files_path)
+    _logger.info(str(result))
+    assert result is not None, "Benchmark failed"
+    assert len(result["features"]) > 0, "No features extracted"
