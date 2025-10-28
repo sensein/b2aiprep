bc7b6cde902098b6490ec74b94ae48c124605d9e
Evan Ng <evan.ng@sickkids.ca>
2025-10-28 17:23:07 -0400

    updated test

--- tests/test_prepare.py
+++ tests/test_prepare.py
@@ -278,13 +278,14 @@ def test_update_metadata_record_and_session_id():
             {"linkId": "participant_id", "answer": [
                 {"valueString": "f7ab1291"}]},
             {"linkId": "session_id", "answer": [
-                {"valueString": "5d9e34a7"}]},
+                {"valueString": "1"}]},
             {"linkId": "participant_id", "answer": [
                 {"valueString": "c7f3a128"}]},
         ]
     }
     ids_to_remap = {"1a3f7e22-8d4b-4e3a-b36d-12a5c2e5b9d8": "f7ab1291-f90c-4a46-94cc-f2b6c1b3e5f7"}
-    update_metadata_record_and_session_id(metadata=metadata, ids_to_remap=ids_to_remap)
+    participant_session_id_to_remap = {"5d9e34a7-2c90-44bf-8b53-1bde7a67e3f2": "1"}
+    update_metadata_record_and_session_id(metadata=metadata, ids_to_remap=ids_to_remap, participant_session_id_to_remap=participant_session_id_to_remap)
     assert metadata == expected_metadata
 
 
