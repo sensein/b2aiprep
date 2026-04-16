c7e5fbf175ee07eb2827f7f53ebad474923076ed
Evan Ng <evan.ng@sickkids.ca>
2026-04-16 12:12:57 -0400

    Updated code based on code review

--- src/b2aiprep/prepare/utils.py
+++ src/b2aiprep/prepare/utils.py
@@ -369,7 +369,8 @@ def load_lookup_table(lookup_path):
 
     with open(lookup_path, "r") as f:
         lookup_table = json.load(f)
-
+        existing_ids = len(lookup_table)
+        _LOGGER.info(f"Pre-existing table contaings {existing_ids} ids")
     remapped_ids = set(lookup_table.values())
 
     return lookup_table, remapped_ids
